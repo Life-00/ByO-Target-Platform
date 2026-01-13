1845d559a02f4ae197e52a6843492ee67c05a180
rocodrama <rocodrama14@gmail.com>
2026-01-13 17:26:19 +0900

    v2/frontend/final

--- frontend/public/config.js
+++
@@ -1,3 +0,0 @@
-window._env_ = {
-  API_BASE_URL: "http://localhost:8000/api/v1", // 기본값
-};
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL =
-  window._env_?.API_BASE_URL || import.meta.env.VITE_API_BASE_URL;
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
 
 const api = axios.create({
   baseURL: BASE_URL,
